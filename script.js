ad07a11a3e05f55a10fafab469e3cdff9ae58cab
Arlindo Pereira <nighto@nighto.net>
2013-10-21 01:18:21 -0200

    Escolas Municipais e NAEIs

--- script.js
+++ script.js
@@ -8,9 +8,7 @@ var MAP_DATA_URL = "data.json";
 var CloudMade_API_key = "ea7350f1880845eabc4585641710ea28";
 var initialLatLon = [-22.941,-43.396];
 var initialZoomLevel = 11;
-var mapData,     // object containing JSON response from Google Fusion Tables
-    old_update,  // UTC date string of last update to compare with current one
-    map,         // the leaflet map instance
+var map,         // the leaflet map instance
     DefaultIcon; // IconClass to extend with colors
 
 $(document).ready(function(){
@@ -62,22 +60,29 @@ function initializeMap(){
 
 function getMapData(){
     $.getJSON(MAP_DATA_URL, function(data){
-        mapData = data;
-
-        processMapData(mapData);
+        processMapData(data);
     });
-}
+};
 
 function processMapData(mapData){
     // defining arrays to store the correspondent points
-    var creche  = [];
+    var CC   = [];
+    var EM   = [];
+    var NAEI = [];
+    var UMEI = [];
     
     for(var i=0, len=mapData.objects.length; i<len; i++){
         var category;
 
         switch(mapData.objects[i].type){ // column with the type
             case "Creche Comunitária":
-                category = creche; break;
+                category = CC; break;
+            case "Escola Municipal":
+                category = EM; break;
+            case "Núcleo Avançado de Educação Infantil":
+                category = NAEI; break;
+            case "Unidade Municipal de Educação Infantil":
+                category = UMEI; break;
         }
 
         if( category !== undefined )
@@ -85,11 +90,17 @@ function processMapData(mapData){
     }
 
     // defining layers with the pins and their icons
-    var lCreche = addPins(creche, 'nursery');
+    var lCC   = addPins(CC, 'nursery');
+    var lEM   = addPins(EM, 'school');
+    var lNAEI = addPins(NAEI, 'school-red');
+    var lUMEI = addPins(UMEI, 'school-green');
 
     // adding them to the list of layers
     var overlayMaps = {
-        "Creche Comunitária": lCreche
+        "Creche Comunitária":                     lCC,
+        "Escola Municipal":                       lEM,
+        "Núcleo Avançado de Educação Infantil":   lNAEI,
+        "Unidade Municipal de Educação Infantil": lUMEI
     };
 
     // and adding the list to map
@@ -106,6 +117,9 @@ function addPins(elements, id){
         var title = elements[i].name;
         var text  = elements[i].address;
         var popupText = '<b>'+title+'</b><br>'+text;
+
+        if(elements[i].aproximate_position !== undefined && elements[i].aproximate_position == 'yes')
+            popupText += '<br>(Posição Aproximada)';
 
         var elementIcon = new DefaultIcon({iconUrl: 'img/'+id+'.png'});
 
